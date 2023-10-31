2f98dbaa36e6bbc6b8711d8f30a5433af53d832a
Rizwan Ahmad <113388795+Riz1981@users.noreply.github.com>
2023-10-31 11:48:30 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+//stores name in an array simply
+let arr = ["Rafia", "Sara", "Mona", "Maria", "Heema"];
+console.log("Names of Friends");
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
---
+++ index.ts
@@ -0,0 +1,14 @@
+//stores name in an array simply
+let arr:string[]=["Rafia","Sara","Mona","Maria","Heema"];
+console.log("Names of Friends");
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+
+
+
+
+
